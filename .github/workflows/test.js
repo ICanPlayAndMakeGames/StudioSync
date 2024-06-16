23d91f1f59fc1bb82dbeeb9e2cdc2b2145f9d846
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2024-06-16 14:06:29 +0100

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,4 +1,4 @@
 const fs = require('fs'); 
-fs.writeFileSync('example.txt', 'This is an example file created by GitHub Actions');
-
+fs.writeFileSync('example1.txt', 'This is an example file created by GitHub Actions');
+console.log("Hey")
 fetch("https://selective-proud-club.glitch.me/")
