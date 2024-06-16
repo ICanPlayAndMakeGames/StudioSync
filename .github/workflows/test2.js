39f4a04761302dd576462be6b84426233dd68578
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2024-06-16 20:58:07 +0100

    Update test2.js

--- .github/workflows/test2.js
+++ .github/workflows/test2.js
@@ -1,3 +1,16 @@
+const fs = require('fs')
 const files = process.env.Changed_Files
+
+fs.readFile(fullPath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    process.exit(1); // Exit with error code
+  } else {
+    console.log('File contents:');
+    console.log(data);
+    // You can process the file contents further here if needed
+  }
+});
+
 
 console.log(files)
