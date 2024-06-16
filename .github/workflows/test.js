47826aaf1f04d33d14fd8b03b57b7bf2b52b1bf7
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2024-06-16 14:24:50 +0100

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,20 +1,32 @@
 const fs = require('fs'); 
 const { execSync } = require('child_process');
 
-// Get the GitHub token from environment variables
+// Environment variables from GitHub Actions
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-console.log(GITHUB_TOKEN)
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
+const GITHUB_ACTOR = process.env.GITHUB_ACTOR;
+const GITHUB_EVENT_NAME = process.env.GITHUB_EVENT_NAME;
+const GITHUB_REF = process.env.GITHUB_REF;
+const GITHUB_SHA = process.env.GITHUB_SHA;
+
+// Check if the push event is from the github-actions[bot]
+if (GITHUB_EVENT_NAME === 'push' && GITHUB_ACTOR === 'github-actions[bot]') {
+  console.log('Stop execution: Push event from github-actions[bot]');
+  process.exit(0);
+}
+
 // Create example.txt
-fs.writeFileSync('example.txt', 'This is an example file created by GitHub Actions');
+const filePath = 'example.txt';
+const content = 'This is an example file created by GitHub Actions';
+fs.writeFileSync(filePath, content);
 
 // Configure Git
 execSync('git config --global user.name "github-actions[bot]"');
 execSync('git config --global user.email "github-actions[bot]@users.noreply.github.com"');
-execSync(`git remote set-url origin https://${GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`);
 
 // Add, commit, and push the file
 execSync('git add example.txt');
 execSync('git commit -m "Add example.txt created by GitHub Actions"');
-execSync('git push');
+execSync(`git push https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git ${GITHUB_REF}:${GITHUB_REF}`);
 
 fetch("https://selective-proud-club.glitch.me/")
