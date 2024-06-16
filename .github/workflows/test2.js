88c7a42b024c28fd28cd96b0643dd143e5c8155c
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2024-06-16 21:17:59 +0100

    Update test2.js

--- .github/workflows/test2.js
+++ .github/workflows/test2.js
@@ -11,7 +11,7 @@ fs.readFile(files, 'utf8', (err, data) => {
    try{
      fetch("https://selective-proud-club.glitch.me/UpdateF",{
        method: 'POST',
-       body: JSON.stringify({content:data})
+       body: "Hey"
      })
    }catch{
      console.error("idk")
