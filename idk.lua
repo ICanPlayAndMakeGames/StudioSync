a7ec08bda9d6b548e74b8d839ba6618a3664d184
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2024-06-18 20:54:36 +0000

    deleted both files

--- idk.lua
+++
@@ -1,4 +0,0 @@
-print("Hey")
-print("yo")
-print("Why you do this")
-local this = game.ReplicatedFirst:WaitForChild("Hey")
--- this2.lua
+++
@@ -1,3 +0,0 @@
-print("yo yo")
-print("What up yo")
-local this = game.ReplicatedFirst:WaitForChild("OOOF")
