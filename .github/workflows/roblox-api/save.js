649bee18d9bd85bb6470d2234d6ef3fd504b3365
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2024-06-22 08:49:43 +0000

    axious

--- .github/workflows/roblox-api/save.js
+++ .github/workflows/roblox-api/save.js
@@ -1,4 +1,4 @@
-const axios = require('dependencies/node_modules/axios/lib/axios')
+const axios = require('dependencies/node_modules/axios/index.js')
 
 const getOperationUrl = "https://apis.roblox.com/cloud/v2/:operationPath";
 
