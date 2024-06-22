f816714ddcad545807edbd2384a6b56885243ad3
ICanPlayAndMakeGames <ICanPlayAndMakeGames@users.noreply.github.com>
2024-06-22 14:29:14 +0000

    Automated Change

---
+++ Game/ServerScriptService/3eee9158-ccf1-c574-0688-51b30000a0c7/Source.lua
@@ -0,0 +1,1 @@
+print("Jeffrey was here")
--- Game/ServerScriptService/58f73231-8de3-b8ee-0684-db9b00009cca/Source.lua
+++ Game/ServerScriptService/58f73231-8de3-b8ee-0684-db9b00009cca/Source.lua
@@ -8,3 +8,5 @@ print("yo yo how you all doing")
 print("yo what up people")
 print("ay ay ay")
 print("ay ay 2222aa2aa2")
+
+print("Why do I do this to myself")
